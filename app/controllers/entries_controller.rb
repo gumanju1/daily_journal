4e3b82a1250f7c871088836af7d425753002f913
sweta <sweeta.gumanju@gmail.com>
2026-07-27 16:48:16 +0545

    Add edit and update journal entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,20 +3,38 @@ class EntriesController < ApplicationController
     @entries = Entry.all.order(created_at: :desc)
   end
 
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
   def new
     @entry = Entry.new
   end
 
   def create
-  @entry = Entry.new(entry_params)
-  @entry.user = current_user
+    @entry = Entry.new(entry_params)
+    @entry.user = User.first
 
-  if @entry.save
-    redirect_to entries_path, notice: "Journal entry created successfully!"
-  else
-    render :new, status: :unprocessable_entity
+    if @entry.save
+      redirect_to entries_path, notice: "Journal entry created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-end
+
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+
+    if @entry.update(entry_params)
+      redirect_to entry_path(@entry), notice: "Journal entry updated successfully!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
 

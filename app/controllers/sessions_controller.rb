22910326fcdc55eac0099b477ade7daa8bcf59d6
sweta <sweeta.gumanju@gmail.com>
2026-07-28 14:57:45 +0545

    Add authentication pages and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_path
-    else
-      flash[:alert] = "Invalid email or password"
-      render :new
-    end
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path
   end
 end
